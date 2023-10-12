2ee96c5e74480a1eb14288193938ae97fab88f59
nodoka <nonchan.t526@gmail.com>
2023-10-12 19:01:53 +0900

    display wave

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,10 @@
 module Main (main) where
 
-import Lib
+import Graphics.Gloss
+import SquareLimit
 
 main :: IO ()
-main = someFunc
+main = display (InWindow "square limit" (640, 480) (100, 100)) white (wave frame0)
+
+frame0 :: Frame
+frame0 = Frame (0, 0) (100, 0) (0, 100)
